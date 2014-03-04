45a255166b881b238a0c498d4aae7c7bad46fe37
exos <tioscar@gmail.com>
2014-03-04 13:45:45 -0300

    anda

--- index.js
+++ index.js
@@ -1,4 +1,55 @@
 
+var FDialog = require('./lib/fdialog');
+
+/** @module Fdialogs */
 module.exports = {
-    FDialog: require('./lib/fdialog')
+
+    FDialog: FDialog,
+
+    /** Display dialog and get Path */
+    getFilePath: function (cb) {
+        var fd = new FDialog();
+        
+        fd.getFilePath(cb);
+
+    },
+
+    /** Display dialog and return content and path of a file */
+    readFile: function (defaultPath, cb) {
+    
+        if (typeof defaultPath == 'function') {
+            cb = defaultPath;
+            defaultPath = null;
+        }
+
+        var fd = new FDialog({
+            path: defaultPath,
+            type: 'open'
+        });
+
+        fd.readFile(cb);
+
+    },
+
+    /** Display dialog and save content in a file */
+    saveFile: function (data,defaultPath, cb) {
+
+        if (!Buffer.isBuffer(data))
+            throw new Error("Data should be a Buffer");
+
+
+        if (typeof defaultPath == 'function') {
+            cb = defaultPath;
+            defaultPath = null;
+        }
+
+        var fd = new FDialog({
+            type: 'save',
+            defaultSavePath: defaultPath
+        });
+
+        fd.saveFile(cb);
+
+    }  
+
 };
